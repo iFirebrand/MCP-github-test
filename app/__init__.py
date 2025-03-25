440b947ecc82fecb88c19b0dec2903aff5f40ec7
Constantin <35843683+iFirebrand@users.noreply.github.com>
2025-03-25 10:34:24 -0400

    Add app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
